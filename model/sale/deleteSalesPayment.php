84880b252415ad3eaad167e6a32bf36fcdaed232
rukshika madushani <rukshika.madushani@atlinkcom.com>
2021-08-29 11:33:58 +0530

    completed sales order payments

--- model/sale/deleteSalesPayment.php
+++ model/sale/deleteSalesPayment.php
@@ -7,36 +7,36 @@
 	if(isset($_POST['paymentId'])){
 
         // Check if the item is in the database
-        $paymentSql = 'SELECT * FROM purchaseorderpayment WHERE id=:id';
+        $paymentSql = 'SELECT * FROM salesorderpayment WHERE id=:id';
         $paymentStatement = $conn->prepare($paymentSql);
         $paymentStatement->execute(['id' => $paymentId]);
         $paymentRow = $paymentStatement->fetch(PDO::FETCH_ASSOC);
         $paymentType = $paymentRow['type'];
         $chequeStatus = $paymentRow['chequeStatus'];
         $paymentAmount = $paymentRow['amount'];
-        $orderId = $paymentRow['purchaseOrderID'];
+        $orderId = $paymentRow['salesOrderID'];
 
         if($paymentStatement->rowCount() > 0){
             
             // payment exists in DB. Hence start the DELETE process
-            $deletePaymentSql = 'UPDATE purchaseorderpayment SET isDeleted = :isDeleted where id= :id';
+            $deletePaymentSql = 'UPDATE salesorderpayment SET isDeleted = :isDeleted where id= :id';
             $deletePaymentStatement = $conn->prepare($deletePaymentSql);
             $deletePaymentStatement->execute(['isDeleted' => true, 'id' => $paymentId]);
 
             if($paymentType  === 'Cash' || $chequeStatus === "Deposited"){
-				$selectPurchaseSql = 'SELECT paidAmount FROM purchaseorder WHERE purchaseID = :orderId';
-				$selectPurchaseStatement = $conn->prepare($selectPurchaseSql);
-				$selectPurchaseStatement->execute(['orderId' => $orderId]);
-				$row = $selectPurchaseStatement->fetch(PDO::FETCH_ASSOC);
+				$selectSalesSql = 'SELECT paidAmount FROM salesorder WHERE saleID = :orderId';
+				$selectSalesStatement = $conn->prepare($selectSalesSql);
+				$selectSalesStatement->execute(['orderId' => $orderId]);
+				$row = $selectSalesStatement->fetch(PDO::FETCH_ASSOC);
 	
 				$paidAmount = 0;
-				if($selectPurchaseStatement->rowCount() > 0){
+				if($selectSalesStatement->rowCount() > 0){
 					$paidAmount = $row['paidAmount'];
 				}
 
-				$updatePurchaseSql = 'UPDATE purchaseorder SET paidAmount = :paidAmount where purchaseID = :orderId';
-				$updatePurchaseStatement = $conn->prepare($updatePurchaseSql);
-				$updatePurchaseStatement->execute(['orderId' => $orderId, 'paidAmount' => ( $paidAmount - $paymentAmount )]);
+				$updateSalesSql = 'UPDATE salesorder SET paidAmount = :paidAmount where saleID = :orderId';
+				$updateSalesStatement = $conn->prepare($updateSalesSql);
+				$updateSalesStatement->execute(['orderId' => $orderId, 'paidAmount' => ( $paidAmount - $paymentAmount )]);
 			}
 
             echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Payment deleted.</div>';
--- model/sale/insertSalesPayment.php
+++ model/sale/insertSalesPayment.php
@@ -11,30 +11,29 @@
 		$remainingAmount = htmlentities($_POST['remainingAmount']);
 		$paymentNote = htmlentities($_POST['note']);
 
-		
 		// Check if mandatory fields are not empty
 		if( isset($orderId) && isset($paymentAmount) && isset($paymentDate)){		
 			
 			// insert purchase order payment
-			$insertPOPaymentSql = 'INSERT INTO purchaseorderpayment (purchaseOrderID, amount, date, type, chequeNo, realisationDate, note, chequeStatus) VALUES(:orderId, :amount, :date, :type, :chequeNo, :realisationDate, :note, "Received" )';
-			$insertPOStatement = $conn->prepare($insertPOPaymentSql);
-			$insertPOStatement->execute(['orderId' => $orderId, 'amount' => $paymentAmount, 'date' => $paymentDate,  'type' => $paymentType,  'chequeNo' => $chequeNo, 'realisationDate' => $chequeDate, 'note' => $paymentNote]);
+			$insertSOPaymentSql = 'INSERT INTO salesorderpayment (salesOrderID, amount, date, type, chequeNo, realisationDate, note, chequeStatus) VALUES(:orderId, :amount, :date, :type, :chequeNo, :realisationDate, :note, "Received" )';
+			$insertSOStatement = $conn->prepare($insertSOPaymentSql);
+			$insertSOStatement->execute(['orderId' => $orderId, 'amount' => $paymentAmount, 'date' => $paymentDate,  'type' => $paymentType,  'chequeNo' => $chequeNo, 'realisationDate' => $chequeDate, 'note' => $paymentNote]);
 
 			// update purchase order paid amount if payment type is cash.
 			if($paymentType === 'Cash'){
-				$selectPurchaseSql = 'SELECT paidAmount FROM purchaseorder WHERE purchaseID = :orderId';
-				$selectPurchaseStatement = $conn->prepare($selectPurchaseSql);
-				$selectPurchaseStatement->execute(['orderId' => $orderId]);
-				$row = $selectPurchaseStatement->fetch(PDO::FETCH_ASSOC);
+				$selectSalesSql = 'SELECT paidAmount FROM salesorder WHERE saleID = :orderId';
+				$selectSalesStatement = $conn->prepare($selectSalesSql);
+				$selectSalesStatement->execute(['orderId' => $orderId]);
+				$row = $selectSalesStatement->fetch(PDO::FETCH_ASSOC);
 	
 				$paidAmount = 0;
-				if($selectPurchaseStatement->rowCount() > 0){
+				if($selectSalesStatement->rowCount() > 0){
 					$paidAmount = $row['paidAmount'];
 				}
 
-				$updatePurchaseSql = 'UPDATE purchaseorder SET paidAmount = :paidAmount where purchaseID = :orderId';
-				$updatePurchaseStatement = $conn->prepare($updatePurchaseSql);
-				$updatePurchaseStatement->execute(['orderId' => $orderId, 'paidAmount' => ($paymentAmount + $paidAmount)]);
+				$updateSalesSql = 'UPDATE salesorder SET paidAmount = :paidAmount where saleID = :orderId';
+				$updateSalesStatement = $conn->prepare($updateSalesSql);
+				$updateSalesStatement->execute(['orderId' => $orderId, 'paidAmount' => ($paymentAmount + $paidAmount)]);
 			}
 
 		} else {
